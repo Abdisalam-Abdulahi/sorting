5b3f26c8dea05aeafce7b7adeffaa8087307dbdd
Abdisalam Abdulahi <iidlex99@gmail.com>
2023-09-26 18:46:35 +0300

    3-quick_sort.c

--- sort.h
+++ sort.h
@@ -24,4 +24,10 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void swap(listint_t *node1, listint_t *node2);
 void selection_sort(int *array, size_t size);
+
+void swap_ints(int *a, int *b);
+int lomuto_partition(int *array, size_t size, int left, int right);
+void lomuto_sort(int *array, size_t size, int left, int right);
+void quick_sort(int *array, size_t size);
 #endif
+
