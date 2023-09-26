2fb5a506aa87a6d20e04e8a7d170bd0d7743af42
Abdisalam Abdulahi <iidlex99@gmail.com>
2023-09-26 11:46:38 +0300

    2-selection_sort.c

---
+++ 2-selection_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+  *selection_sort -implementing selection sort algorith
+  *@array: the array to be sorted
+  *@size: the size of the array
+  */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i = 0;
+	size_t j;
+	int position;
+	int temp;
+	int small;
+
+	while (i < size)
+	{
+		small = array[i];
+		for (j = i; j < size; j++)
+		{
+			if (array[j] <= small)
+			{
+				small = array[j];
+				position = j;
+			}
+		}
+
+		temp = array[i];
+		array[i] = small;
+		array[position] = temp;
+		print_array(array, size);
+		i++;
+	}
+}
